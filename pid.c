f56fb8f3a5fba653f18dce3ae8c2abbb6a3634c5
sebastian <sebastian@momentos.dk>
2014-11-30 10:25:15 +0100

    slkjfd

--- pid.c
+++ pid.c
@@ -1,15 +1,21 @@
+#include <string.h>
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <wiringPi.h>
 
 
 int getTemp();
 void setCorrection();
 
-int setTemp;
-int setTime = 0;
+float error;
+float temp;
+float setTemp;
+int setTime = 60000;
 int secPassed = 0;
 float powerOut;
+int pwm;
+
 
 int main(void)
 {
@@ -24,30 +30,52 @@ int main(void)
         unsigned hhPassed = secPassed / (60*60);
         unsigned mmPassed = (secPassed - hhPassed*60*60) / 60;
         printf("time passed is %u hours :  %u minutes : %u seconds\n", hhPassed, mmPassed,ssPassed);
-        printf("curent temperature is %d\n", getTemp());
+        getTemp();
+        printf("curent temperature is %f\n", temp);
         sleep(1);
     }
 }
 
+void setOut() {
+  if (wiringPiSetup () == -1)
+    return 1 ;
 
-{
-    }
+  pinMode (0, OUTPUT) ;         // aka BCM_GPIO pin 17
 
-    int temp;
+  if(temp - setTemp < 0) {
+  {
+    digitalWrite (0, 1) ;       // On
+    delay (500) ;
+  }
+  else{
+    digitalWrite (0, 0) ;       // On
+  }
+  return 0 ;
+}
+
+
+
+
+
+
+
+int getTemp() {
+    char tmp[5];
+//    fp = fopen("/sys/bus/w1/devices/28-000005f5dac4/w1_slave", "r");
     FILE *fp;
+    fp = fopen("w1_slave", "r");
+    char buf[80];
 
-    fp = fopen("/sys/bus/w1/devices/28-000005f5dac4/w1_slave", "r");
-
-    if(fp == -1) {
+    if(fp == 0) {
         perror ("Couldn't open the w1 device.");
         return 1;
     }
-    int c;
-    while(((c = getc(fp)) !=EOF){
-        putchar(c);
+    fgets(buf, 80, fp);
+    if(strstr(buf, "YES") != NULL){
+        fgets(buf, 80, fp);
+        strncpy(tmp, strstr(buf, "t=") + 2, 5);
+        temp = atoi(tmp) / 1000.0;
     }
-//    rewind(fp);
-//    fscanf(fp, " %d ", &temp);
     fclose(fp);
-//    return temp;
+    return temp;
 }
