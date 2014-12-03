3036e8d4e9ca5a6bf2443047b3d57a3af05d118f
sebastian <sebatian@momentos.dk>
2014-12-03 00:53:35 +0000

    sdfaf

--- pid.c
+++ pid.c
@@ -8,6 +8,7 @@
 int getTemp();
 void setCorrection();
 int setOut();
+void pidcalc();
 
 // pid variables
 float e; // error value sp - pv
@@ -18,7 +19,7 @@ float kd = 1; // deavirate gain
 float pv; // tempereture
 float pv_1 = 0; //forige temp
 float pv_2 = 0; //forige af forige temp
-
+float co = 0; // controller output
 int secPassed = 0;
 
 // cycletime for powerOut
@@ -43,48 +44,49 @@ int main(void)
         unsigned mmPassed = (secPassed - hhPassed*60*60) / 60;
         printf("time passed is %u hours :  %u minutes : %u seconds\n", hhPassed, mmPassed,ssPassed);
         getTemp();
+        printf("1");
         pidCalc();
+        printf("");
         setOut();
-        printf("curent temperature is %f\n", pv);
-        sleep(1);
+        printf("e %f\n ei %f\n kp %f\n ki %f\n kd %f\n pv %f\n pv_1 %f\n pv_2 e %f\n co %f\n", e, ei, kp, ki, kd, pv, pv_1, pv_2, co);
     }
 }
 
 int pidCalc(){
   e = sp - pv;
-  ei += e:
+  ei += e;
   float p = kp * (pv_1 - pv);
   float i = ki*ei;
-  float d = dk * (2.0 * pv_1 - pv - pv_2);
+  float d = kd * (2.0 * pv_1 - pv - pv_2);
 }
 
 int setOut()
 {
   wiringPiSetup () ;
   pinMode (0, OUTPUT) ;
-  while(true){
-    int timeHigh = pv;
+//  while(1){
+    int timeHigh = co;
     int timeLow = cycleTime - timeHigh;
-    if(poweOut > 0 ){
+    if(co > 0 ){
       digitalWrite (0, HIGH) ; delay (timeHigh) ;
       digitalWrite (0, LOW) ; delay (timeLow) ;
     }
     else{
       digitalWrite (0,  LOW) ; delay (2000) ;
     }
-  }
+//  }
     return 0 ;
 }
 
 
 int getTemp() {
+
     char tmp[5];
     FILE *fp;
     fp = fopen("/sys/bus/w1/devices/28-000005f5dac4/w1_slave", "r");
 
 //    fp = fopen("w1_slave", "r");
     char buf[80];
-
     if(fp == 0) {
         perror ("Couldn't open the w1 device.");
         return 1;
@@ -96,5 +98,5 @@ int getTemp() {
         pv = atoi(tmp) / 1000.0;
     }
     fclose(fp);
-    return temp;
+    return 1;
 }
