f86eb21cc7a7dbd7cc1bda72198a4ed842fc7af9
sebastian <sebastian@momentos.dk>
2014-12-03 01:04:28 +0100

    added pid algorithm

--- pid.c
+++ pid.c
@@ -8,14 +8,26 @@
 int getTemp();
 void setCorrection();
 int setOut();
-float error;
-float temp;
-float setTemp = 63.0;
-int setTime = 60000;
-int secPassed = 0;
-float powerOut;
-int pwm;
 
+// pid variables
+float e; // error value sp - pv
+float ei; // error arkumulated
+float kp = 1; //proportional gain
+float ki = 1; //integral gain
+float kd = 1; // deavirate gain
+float pv; // tempereture
+float pv_1 = 0; //forige temp
+float pv_2 = 0; //forige af forige temp
+
+int secPassed = 0;
+
+// cycletime for powerOut
+int cycleTime = 2000;
+
+
+// user setings
+float sp = 63.0; // set point
+int setTime = 60000;
 
 int main(void)
 {
@@ -31,53 +43,38 @@ int main(void)
         unsigned mmPassed = (secPassed - hhPassed*60*60) / 60;
         printf("time passed is %u hours :  %u minutes : %u seconds\n", hhPassed, mmPassed,ssPassed);
         getTemp();
+        pidCalc();
         setOut();
-        printf("curent temperature is %f\n", temp);
+        printf("curent temperature is %f\n", pv);
         sleep(1);
     }
 }
 
+int pidCalc(){
+  e = sp - pv;
+  ei += e:
+  float p = kp * (pv_1 - pv);
+  float i = ki*ei;
+  float d = dk * (2.0 * pv_1 - pv - pv_2);
+}
 
-
-int setOut ()
+int setOut()
 {
   wiringPiSetup () ;
   pinMode (0, OUTPUT) ;
-  int i = 0;
-  if(temp - setTemp < 0 ){
-    digitalWrite (0, HIGH) ; delay (1000) ;
+  while(true){
+    int timeHigh = pv;
+    int timeLow = cycleTime - timeHigh;
+    if(poweOut > 0 ){
+      digitalWrite (0, HIGH) ; delay (timeHigh) ;
+      digitalWrite (0, LOW) ; delay (timeLow) ;
+    }
+    else{
+      digitalWrite (0,  LOW) ; delay (2000) ;
+    }
   }
-  else{
-    digitalWrite (0,  LOW) ; delay (1000) ;
-  }
-  return 0 ;
+    return 0 ;
 }
-
-
-/*
-int setOut() {
-  wiringPiSetup () ;
-  pinMode (0, OUTPUT) ;
-
-  pinMode (0, OUTPUT) ;         // aka BCM_GPIO pin 17
-
-  if(temp - setTemp < 0) {
-        printf("set high");
-    digitalWrite (0, HIGH) ; delay (500) ;
-    delay (1000) ;
-  }
-  else{
-    digitalWrite (0,  LOW) ; delay (500) ;
-    delay (1000) ;
-  }
-  return 0 ;
-
-}
-*/
-
-
-
-
 
 
 int getTemp() {
@@ -96,7 +93,7 @@ int getTemp() {
     if(strstr(buf, "YES") != NULL){
         fgets(buf, 80, fp);
         strncpy(tmp, strstr(buf, "t=") + 2, 5);
-        temp = atoi(tmp) / 1000.0;
+        pv = atoi(tmp) / 1000.0;
     }
     fclose(fp);
     return temp;
